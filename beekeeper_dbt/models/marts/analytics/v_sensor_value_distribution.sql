3270bdc816994ba5720566cece3c19ed71145f2c
XinGclover <cindygao7809@gmail.com>
2026-04-25 12:26:48 +0200

    add sensor name and lable in dashboard

--- beekeeper_dbt/models/marts/analytics/v_sensor_value_distribution.sql
+++ beekeeper_dbt/models/marts/analytics/v_sensor_value_distribution.sql
@@ -1,5 +1,8 @@
 select
-    measured_value,
-    sensor_key,
-    measured_at
-from {{ ref('fact_sensor_measurements') }}
+    fsm.measured_value,
+    fsm.sensor_key,
+    fsm.measured_at,
+    ds.sensor_name,
+    ds.sensor_label
+from {{ ref('fact_sensor_measurements') }} fsm
+left join {{ ref('dim_sensor') }} ds on fsm.sensor_key = ds.sensor_key
--- beekeeper_dbt/models/marts/dimensions/dim_sensor.sql
+++ beekeeper_dbt/models/marts/dimensions/dim_sensor.sql
@@ -6,6 +6,18 @@ with sensor as (
 sensor_type as (
     select *
     from {{ ref('stg_sensor_type') }}
+),
+
+hive as (
+    select *
+    from {{ ref('dim_hive') }}
+    where is_current = true
+),
+
+apiary as (
+    select *
+    from {{ ref('dim_apiary') }}
+    where is_current = true
 )
 
 select
@@ -22,7 +34,15 @@ select
     case
         when s.dbt_valid_to is null then true
         else false
-    end as is_current
+    end as is_current,
+    'Sensor ' || s.sensor_id as sensor_name,
+    'Hive ' || s.hive_id || ' - Sensor ' || s.sensor_id as sensor_label,
+    'Hive ' || h.hive_id as hive_name,
+    'Apiary ' || a.apiary_id || ' (' || coalesce(a.city, 'Location') || ')' as apiary_name
 from sensor s
 left join sensor_type st
     on s.sensor_type_id = st.sensor_type_id
+left join hive h
+    on s.hive_id = h.hive_id
+left join apiary a
+    on h.apiary_id = a.apiary_id
